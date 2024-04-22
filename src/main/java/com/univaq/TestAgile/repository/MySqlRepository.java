2ea7cadf857c8b74aaeb4a44ee079ef55e47d4ad
MarcoMocerino <104770519+MarcoMocerino@users.noreply.github.com>
2024-04-22 17:24:30 +0200

    Pom aggiornato

--- src/main/java/com/univaq/TestAgile/repository/MySqlRepository.java
+++
@@ -1,8 +0,0 @@
-package com.univaq.TestAgile.repository;
-
-import com.univaq.TestAgile.model.Pianta;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MySqlRepository extends JpaRepository<Pianta,Integer> {
-
-}
