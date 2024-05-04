20ffdaee0bc21fef66bf063832842735e18b7b50
Giovanni Altieri <79712043+G-Altieri@users.noreply.github.com>
2024-05-04 23:45:22 +0200

    inizio template grafico
    Aggiunte variabili dei colori al template css e il primo componente dei bottoni

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,25 @@ module.exports = {
     "./src/main/resources/templates/**/**/*.html"
   ],
   theme: {
-    extend: {},
+      extend: {
+          colors: {
+              'verde1': '#004322',
+              'giallo1': '#FED860',
+              'verde2': '#BCE3A0',
+              'bianco1': '#F8E7C2',
+              'rosso1': '#F3896B',
+              'celeste1': '#80D6E5',
+              'giallo2': '#FFD741',
+              'verdeSuccessoBG': '#BCF0B4',
+              'verdeSuccessoText': '#3B693A',
+              'rossoInSuccessoBg': '#FFDBC9',
+              'rossoInSuccessoText': '#341000',
+              'gialloSecondarioText': '#261A00',
+              'gialloSecondarioBG': '#FFE093',
+              'bianco2': '#FFF7DF',
+              'viola1': '#8E80E5',
+          },
+      },
   },
   plugins: [],
 }
