f16a3b99e0949561d1388e91ca4d74b1d5fd130f
Giovanni Altieri <79712043+G-Altieri@users.noreply.github.com>
2024-05-15 16:01:02 +0200

    inizio integrazione zolla - orto

--- src/main/java/com/univaq/TestAgile/controller/OrtoReferenteController.java
+++ src/main/java/com/univaq/TestAgile/controller/OrtoReferenteController.java
@@ -2,7 +2,9 @@ package com.univaq.TestAgile.controller;
 
 import com.univaq.TestAgile.model.Evento;
 import com.univaq.TestAgile.model.OrtoReferente;
+import com.univaq.TestAgile.model.Zolla;
 import com.univaq.TestAgile.repository.OrtoReferenteRepository;
+import com.univaq.TestAgile.repository.ZollaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +18,10 @@ public class OrtoReferenteController {
     @Autowired
     private OrtoReferenteRepository ortoReferenteRepository;
 
+    @Autowired
+    private ZollaRepository zollaRepository;
+
+
     @GetMapping("/all")
     public List<OrtoReferente> getAllRichiesteOrti() {
         return (List<OrtoReferente>) ortoReferenteRepository.findAll();
@@ -25,10 +31,12 @@ public class OrtoReferenteController {
     public List<OrtoReferente> getRichiesteOrtiIndefinite() {
         return ortoReferenteRepository.findByStato("indefinito");
     }
+
     @GetMapping("/accettate")
     public List<OrtoReferente> getRichiesteOrtiAccettate() {
         return ortoReferenteRepository.findByStato("accettato");
     }
+
     @GetMapping("/rifiutate")
     public List<OrtoReferente> getRichiesteOrtiRifiutate() {
         return ortoReferenteRepository.findByStato("rifiutato");
@@ -42,17 +50,39 @@ public class OrtoReferenteController {
     }
 
     @GetMapping("/accetta-richiesta/{id}/{stato}")
-    public String accettaRichiestaOrto(@PathVariable Long id,@PathVariable String stato) {
-        System.out.println("Richiesta Accettata"+id);
-        OrtoReferente temp= ortoReferenteRepository.findById(id).get();
-        temp.setStato(stato);
-        ortoReferenteRepository.save(temp);
+    public String accettaRichiestaOrto(@PathVariable Long id, @PathVariable String stato) {
+        System.out.println("Richiesta Accettata" + id);
+        OrtoReferente tempOrto = ortoReferenteRepository.findById(id).get();
+        tempOrto.setStato(stato);
+        //Riempio l'orto con le zolle
+        insertZolleVuote(tempOrto);
+
+        ortoReferenteRepository.save(tempOrto);
+
+
+
         return "redirect:/admin/richieste-orto-referente";
     }
 
     @GetMapping("/get/{id}/")
     public OrtoReferente getEventoById(@PathVariable long id) {
         return ortoReferenteRepository.findById(id).get();
+    }
+
+    @GetMapping("/insertZolleVuote/{orto}/")
+    public void insertZolleVuote(@PathVariable OrtoReferente orto) {
+        int dimensioneX = orto.getDimensioneX();
+        int dimensioneY = orto.getDimensioneY();
+        System.out.println("Mi hai chiamato");
+        System.out.println(orto);
+        System.out.println(dimensioneX);
+        System.out.println(dimensioneY);
+        for (int x = 1; x <= dimensioneX; x++) {
+            for (int y = 1; y <= dimensioneY; y++) {
+                Zolla zolla = new Zolla("Zolla_" + x + "_" + y, "sabbioso", null, null, null, null, null, null);
+                zollaRepository.save(zolla);
+            }
+        }
     }
 
 }
--- src/main/java/com/univaq/TestAgile/model/Zolla.java
+++ src/main/java/com/univaq/TestAgile/model/Zolla.java
@@ -1,5 +1,125 @@
 package com.univaq.TestAgile.model;
 
+import jakarta.persistence.*;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.util.Date;
+
+@EnableJpaRepositories
+@Entity
+@Table(name = "zolla")
 public class Zolla {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "orto_id")
+    private OrtoReferente orto;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "tipo_terreno")
+    private String tipoTerreno;
+
+    @Column(name = "ortaggio")
+    private String ortaggio;
+
+    @Column(name = "semina")
+    private Date semina;
+
+    @Column(name = "stato")
+    private String stato;
+    @Column(name = "data_scadenza")
+    private Date dataScadenza;
+
+    @Column(name = "data_prenotazione")
+    private Date dataPrenotazione;
+
+    @Column(name = "proprietario")
+    private String proprietario;
+
+    public Zolla(String nome, String tipoTerreno, String ortaggio, Date semina, String stato, Date dataScadenza, Date dataPrenotazione, String proprietario) {
+        this.nome = nome;
+        this.tipoTerreno = tipoTerreno;
+        this.ortaggio = ortaggio;
+        this.semina = semina;
+        this.stato = stato;
+        this.dataScadenza = dataScadenza;
+        this.dataPrenotazione = dataPrenotazione;
+        this.proprietario = proprietario;
+    }
+
+    public Zolla() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipoTerreno() {
+        return tipoTerreno;
+    }
+
+    public void setTipoTerreno(String tipoTerreno) {
+        this.tipoTerreno = tipoTerreno;
+    }
+
+    public String getOrtaggio() {
+        return ortaggio;
+    }
+
+    public void setOrtaggio(String ortaggio) {
+        this.ortaggio = ortaggio;
+    }
+
+    public Date getSemina() {
+        return semina;
+    }
+
+    public void setSemina(Date semina) {
+        this.semina = semina;
+    }
+
+    public String getStato() {
+        return stato;
+    }
+
+    public void setStato(String stato) {
+        this.stato = stato;
+    }
+
+    public Date getDataScadenza() {
+        return dataScadenza;
+    }
+
+    public void setDataScadenza(Date dataScadenza) {
+        this.dataScadenza = dataScadenza;
+    }
+
+    public Date getDataPrenotazione() {
+        return dataPrenotazione;
+    }
+
+    public void setDataPrenotazione(Date dataPrenotazione) {
+        this.dataPrenotazione = dataPrenotazione;
+    }
+
+    public String getProprietario() {
+        return proprietario;
+    }
+
+    public void setProprietario(String proprietario) {
+        this.proprietario = proprietario;
+    }
 }
